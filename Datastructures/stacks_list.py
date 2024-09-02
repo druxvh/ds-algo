b26bd5d9550f7861160fc272dbea5b101e45fb2b
dm <druavhm@gmail.com>
2024-09-02 23:28:11 +0530

    add stack using a list

---
+++ Datastructures/stacks_list.py
@@ -0,0 +1,35 @@
+'''
+Stacks using List
+'''
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        else: 
+            raise IndexError("Stack is empty")
+        
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("Stack is empty")
+        
+    def size(self):
+        return len(self.items)
+    
+dm = Stack()
+dm.push(33)
+dm.push(33)
+dm.push(33)
+dm.push(33)
+print(dm)
