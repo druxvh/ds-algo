830fc77d118e1231b90e01ebd13e86abb9ec3631
dm <druavhm@gmail.com>
2024-09-29 00:47:14 +0530

    add selection sort algorithm

---
+++ Algorithms/selection_sort.py
@@ -0,0 +1,16 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+'''
+l = [1,10,5,3,9,7]
+selection_sort(l)
+print(l)
+'''
